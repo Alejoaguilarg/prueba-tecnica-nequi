ed583f6fe9a8f8b457df96af768e5975d09f40eb
Alejoaguilarg <alejo852_@hotmail.com>
2025-08-08 11:55:21 -0500

    Test: entryPoints test

---
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/bancolombia/api/RouterRestTest.java
@@ -0,0 +1,79 @@
+package co.com.bancolombia.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RouterRestTest {
+
+    @Mock
+    private Handler handler;
+
+    private RouterFunction<ServerResponse> routerFunction;
+    private WebTestClient client;
+
+    @BeforeEach
+    void setUp() {
+        routerFunction = new RouterRest().routerFunction(handler);
+
+        client = WebTestClient.bindToRouterFunction(routerFunction).build();
+    }
+
+    @Test
+    void shouldRouteToCreateFranchise() {
+        when(handler.createFranchise(any())).thenReturn(ServerResponse.ok().build());
+
+        client.post()
+                .uri("/api/franchises")
+                .exchange()
+                .expectStatus().isOk();
+
+        verify(handler).createFranchise(any());
+    }
+
+    @Test
+    void shouldRouteToAddBranch() {
+        when(handler.addBranch(any())).thenReturn(ServerResponse.ok().build());
+
+        client.post()
+                .uri("/api/branches")
+                .exchange()
+                .expectStatus().isOk();
+
+        verify(handler).addBranch(any());
+    }
+
+    @Test
+    void shouldRouteToAddProduct() {
+        when(handler.addProduct(any())).thenReturn(ServerResponse.ok().build());
+
+        client.post()
+                .uri("/api/products")
+                .exchange()
+                .expectStatus().isOk();
+
+        verify(handler).addProduct(any());
+    }
+
+    @Test
+    void shouldRouteToDeleteProduct() {
+        when(handler.deleteProduct(any())).thenReturn(ServerResponse.ok().build());
+
+        client.delete()
+                .uri("/api/products/10")
+                .exchange()
+                .expectStatus().isOk();
+
+        verify(handler).deleteProduct(any());
+    }
+}
