a68eab286704dc1a90dfbf7d2b358bd466dbac06
Alejoaguilarg <alejo852_@hotmail.com>
2025-08-07 20:47:27 -0500

    Feature: add Product endpoint, test and entrypoint test

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/entities/ProductEntity.java
@@ -0,0 +1,25 @@
+package co.com.bancolombia.r2dbc.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table("products")
+public class ProductEntity {
+    @Id
+    private Long id;
+    @Column("name")
+    private String name;
+    @Column("stock")
+    private Integer stock;
+    @Column("branch_id")
+    private Long branchId;
+}
